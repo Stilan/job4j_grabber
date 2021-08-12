aaaad970f4d684660d20a4fd4c603d740368fa24
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-12 18:36:03 +0700

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,19 +5,25 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.List;
+/**
+ *  @author Александр Литвинов
+ *  @since 11.08.2021
+ *
+ */
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements elms = doc.select(".postslisttopic");
-        for (Element td : elms) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element element = td.parent();
-            Element tr = element.child(5);
-            System.out.println(tr.text());
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements elms = doc.select(".postslisttopic");
+            for (Element td : elms) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element element = td.parent();
+                Element tr = element.child(5);
+                System.out.println(tr.text());
             }
         }
     }
+}
