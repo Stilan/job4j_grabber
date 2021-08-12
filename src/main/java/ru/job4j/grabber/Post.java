9cb33f11c40434992160b02389c0f83ae27af46a
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-12 18:56:13 +0700

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,16 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+/**
+ *  @author Александр Литвинов
+ *  @since 11.08.2021
+ *
+ */
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+}
