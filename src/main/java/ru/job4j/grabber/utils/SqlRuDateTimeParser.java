06bc0811cd8a279568f862f3110663d6b07b0790
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-13 17:50:36 +0700

    2.1. Преобразование даты [#289476 #189043]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -11,9 +11,11 @@ import java.util.HashMap;
  *
  */
 public class SqlRuDateTimeParser implements DateTimeParser {
+
+     private static final HashMap<String, String> MAP_MONTH = new HashMap<>();
+     private static final  DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d MMMM yy, HH:mm");
     /**
      * Метод принимает строку с дата и преобразует ее в LocalDateTime
-     *
      * @param parse входная строка
      * @return LocalDateTime
      */
@@ -38,21 +40,19 @@ public class SqlRuDateTimeParser implements DateTimeParser {
      */
       private LocalDateTime getLocalDateTime(String strDataSql) {
           String[] strNewMonth = strDataSql.split(" ");
-          HashMap<String, String> map = new HashMap<>();
-          map.put("янв", "января");
-          map.put("фев", "февраля");
-          map.put("мар", "марта");
-          map.put("апр", "апреля");
-          map.put("май", "мая");
-          map.put("июн", "июня");
-          map.put("июл", "июля");
-          map.put("авг", "августа");
-          map.put("сен", "сентября");
-          map.put("окт", "октября");
-          map.put("ноя", "ноября");
-          map.put("дек", "декабря");
-          String strD = strDataSql.replace(strNewMonth[1], map.get(strNewMonth[1]));
-          DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("d MMMM yy, HH:mm");
-          return LocalDateTime.parse(strD, formatter1);
+          MAP_MONTH.put("янв", "января");
+          MAP_MONTH.put("фев", "февраля");
+          MAP_MONTH.put("мар", "марта");
+          MAP_MONTH.put("апр", "апреля");
+          MAP_MONTH.put("май", "мая");
+          MAP_MONTH.put("июн", "июня");
+          MAP_MONTH.put("июл", "июля");
+          MAP_MONTH.put("авг", "августа");
+          MAP_MONTH.put("сен", "сентября");
+          MAP_MONTH.put("окт", "октября");
+          MAP_MONTH.put("ноя", "ноября");
+          MAP_MONTH.put("дек", "декабря");
+          String strD = strDataSql.replace(strNewMonth[1], MAP_MONTH.get(strNewMonth[1]));
+          return LocalDateTime.parse(strD, DATE_TIME_FORMATTER);
       }
 }
