1a36e2365c210f8578686afc01f43b18ed1ddeb0
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-16 18:44:05 +0700

    2.4. SqlRuParse [#285213 #189671]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -11,6 +11,7 @@ import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,15 +20,13 @@ import java.util.List;
  *
  */
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
 
-//    private final DateTimeParser dateTimeParser;
-//    private List<Post> list;
-//
-//
-////    public SqlRuParse(DateTimeParser dateTimeParser) {
-////        this.dateTimeParser = dateTimeParser;
-////    }
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     /**
      * Метод list загружает список всех постов.
@@ -35,53 +34,62 @@ public class SqlRuParse {
      * @param link
      * @return
      */
-//    @Override
-//    public List<Post> list(String link) {
-//        try {
-//            Document document = Jsoup.connect(link).get();
-//            Elements elements1 = document.select(".postslisttopic");
-//            for (Element td : elements1) {
-//                Element href = td.child(0);
-//                Post post  = detail(href.attr("href"));
-//                list.add(post);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return list;
-//    }
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        try {
+            Document document = Jsoup.connect(link).get();
+            Elements listOfPosts = document.select(".postslisttopic");
+            for (Element td : listOfPosts) {
+                Element href = td.child(0);
+                Post post = detail(href.attr("href"));
+               list.add(post);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
     /**
      * Метод detail загружает все детали одного поста
+     *
      * @param link
      * @return
      */
-//    @Override
-//    public Post detail(String link) {
-//        Post post = null;
-//        try {
-//            Document document = Jsoup.connect(link).get();
-//            Elements elements1 = document.select(".messageHeader").eq(0);
-//            Element element1 = elements1.get(0);
-//            Elements elements2 = document.select(".msgBody").eq(1);
-//            Element element2 = elements2.get(0);
-//            Element element3 = document.select(".msgFooter").get(0);
-//            String dateStr  = element3.text().substring(0, element3.text().length() - 53);
-//            DateTimeParser dateTimeParse = new SqlRuDateTimeParser();
-//            post = new Post(1,
-//                    element1.text().substring(4, element1.text().length() - 6),
-//                    link,
-//                    element2.text(),
-//                    dateTimeParse.parse(dateStr));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return post;
-//    }
-//}
+    @Override
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Document document = Jsoup.connect(link).get();
+            Elements header = document.select(".messageHeader").eq(0);
+            Element jobTitle = header.get(0);
+            Elements descriptionList = document.select(".msgBody").eq(1);
+            Element description = descriptionList.get(0);
+            Element footer = document.select(".msgFooter").get(0);
+            String dateStr = footer.text().substring(0, footer.text().length() - 52);
+            post = new Post(
+                    jobTitle.text().substring(4, jobTitle.text().length() - 6),
+                    link,
+                    description.text(),
+                    dateTimeParser.parse(dateStr));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+        public static void main(String[] args) {
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+        List<Post> posts = sqlRuParse.list("https://www.sql.ru/forum/job-offers/1");
+        System.out.println(posts.toString());
+        Post post = sqlRuParse.detail(
+                "https://www.sql.ru/forum/1336939/java-razrabotchik-moskva-udalyonka-200-300-gross");
+        System.out.println(post.toString());
+    }
+}
 //    public static void main(String[] args) throws Exception {
-//        for (int i = 1; i < 6; i++) {
-//            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+//            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
 //            Elements elms = doc.select(".postslisttopic");
 //            for (Element td : elms) {
 //                Element href = td.child(0);
@@ -91,27 +99,29 @@ public class SqlRuParse {
 //                Element tr = element.child(5);
 //                System.out.println(tr.text());
 //            }
-//            System.out.println("_____________________");
 //        }
+//
+//}
 //        isPostDetails(
 //                "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
 //    }
 //
-    /**
-     * метод парсит описание и дату создания
-     * @param strPath
-     * @throws IOException
-     */
-    public static void isPostDetails(String strPath) throws IOException {
-        Document d = Jsoup.connect(strPath).get();
-        Elements getDescriptionList = d.select(".msgBody").eq(1);
-        Element description = getDescriptionList.get(0);
-        System.out.println(description.text());
-        Element dateCreation = d.select(".msgFooter").get(0);
-        System.out.println(dateCreation.text().substring(0, 17));
-    }
-
-    public static void main(String[] args) throws IOException {
-        isPostDetails("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
-    }
-}
+//    /**
+//     * метод парсит описание и дату создания
+//     * @param strPath
+//     * @throws IOException
+//     */
+//    public static void isPostDetails(String strPath) throws IOException {
+//        Document d = Jsoup.connect(strPath).get();
+//        Elements getDescriptionList = d.select(".msgBody").eq(1);
+//        Element description = getDescriptionList.get(0);
+//        System.out.println(description.text());
+//        Element dateCreation = d.select(".msgFooter").get(0);
+//        System.out.println(dateCreation.text().substring(0, 17));
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//        isPostDetails("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+//    }
+//  12 авг 21, 12:28
+//  17 окт 07, 14:42
