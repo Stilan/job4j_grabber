6b1c69ecf3cdbd69a685a24ba1fd1f7f2916284c
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-16 16:03:30 +0700

    2.3. Загрузка деталей поста. [#285212 #189375]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,113 @@
+package ru.job4j.grabber;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+
+    public PsqlStore(Properties cfg) {
+       try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            cnn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        /* cnn = DriverManager.getConnection(...); */
+    }
+    @Override
+    public void save(Post post) {
+     try (PreparedStatement preparedStatement = cnn.prepareStatement("insert into post(namePost, textPost, link, created) values (?, ?, ?, ?)",
+                          Statement.RETURN_GENERATED_KEYS)) {
+         preparedStatement.setString(1, post.getTitle());
+         preparedStatement.setString(2, post.getLink());
+         preparedStatement.setString(3, post.getDescription());
+         LocalDateTime localDateTime = post.getCreated();
+         Timestamp timestamp = Timestamp.valueOf(localDateTime);
+         preparedStatement.setTimestamp(4, timestamp);
+         preparedStatement.execute();
+         try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+             if (generatedKeys.next()) {
+                 post.setId(generatedKeys.getInt(1));
+             }
+         }
+
+     }  catch (Exception e) {
+         e.printStackTrace();
+     }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement preparedStatement = cnn.prepareStatement("select * from post")) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    posts.add(new Post(
+                          resultSet.getInt("id"),
+                          resultSet.getString("namePost"),
+                          resultSet.getString("textPost"),
+                          resultSet.getString("link"),
+                          resultSet.getTimestamp("created").toLocalDateTime()
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post where id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    post = new  Post(
+                            resultSet.getInt("id"),
+                            resultSet.getString("namePost"),
+                            resultSet.getString("textPost"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime()
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("/Users/aleksandrlitvinov/projects/job4j_design/src/main/resources/app.properties")) {
+            cfg.load(in);
+        }
+        PsqlStore psqlStore = new PsqlStore(cfg);
+
+    }
+}
