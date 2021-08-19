8249d7c509c09e16c03c6f6297e8d7ef18ed8fd5
Stilan <40999153+Stilan@users.noreply.github.com>
2021-08-19 13:47:17 +0700

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,10 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import ru.job4j.html.SqlRuParse;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -54,6 +54,36 @@ public class Grabber implements Grab {
         scheduler.scheduleJob(job, trigger);
     }
 
+    /**
+     * метод отправлеет список вакансий на порт 9000
+     * @param store бд хранилище
+     */
+    public void web(Store store) {
+        new Thread(() -> {
+            // серверсокет прослушивает порт 9000
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    // accept() будет ждать пока
+                    //кто-нибудь не захочет подключиться
+                    try (OutputStream out = socket.getOutputStream()) {
+                       // отправлять список вакансий
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            // ответ сервера
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static class GrabJob implements Job {
 
         @Override
@@ -78,6 +108,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(new SqlRuDateTimeParser()), store, scheduler);
-
+        grab.web(store);
     }
 }
