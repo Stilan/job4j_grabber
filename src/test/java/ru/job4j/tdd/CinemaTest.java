31ae26a8aeccd4da9e6d17281ec0cae90faf229a
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-20 20:50:10 +0700

    2. Что такое TDD? [#4918 #204816]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.tdd;
 
 import junit.framework.TestCase;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
@@ -13,6 +14,7 @@ import java.util.Calendar;
 import java.util.List;
 public class CinemaTest  {
 
+    @Ignore
     @Test
     public void buy() {
         Account account = new AccountCinema();
@@ -20,23 +22,46 @@ public class CinemaTest  {
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-  //      assertThat(ticket, is(new Ticket3D()));
+        assertThat(ticket, is(new Ticket3D()));
     }
-
+    @Ignore
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-    //    assertThat(sessions, is(Arrays.asList(new Session3D())));
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
-
+    @Ignore
     @Test
     public void add() {
         Cinema cinema = new Cinema3D();
         Session session = new Session3D();
         cinema.add(session);
-    //    assertThat(cinema.find(session1 -> true), is(session));
+        assertThat(cinema.find(session1 -> true), is(session));
     }
+    @Ignore
+    @Test
+    public void dateEnteredIncorrectly() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(-2020, -10, -10, -23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+    @Ignore
+    @Test
+    public void buyErrorTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+        assertThat(ticket2, is(new Ticket3D()));
+    }
+
 }
 
