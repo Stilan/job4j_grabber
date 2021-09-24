3d3b99e6aba7fba5da5a286c05ec1323873e9d8a
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-24 18:55:17 +0700

    1. Отчеты. [#850 #207794]

---
+++ src/test/java/ru/job4j/srp/ReportAccountingDepartmentTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.srp;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import ru.job4j.ood.srp.Employee;
+import ru.job4j.ood.srp.MemStore;
+import ru.job4j.ood.srp.Report;
+import ru.job4j.ood.srp.ReportAccountingDepartment;
+
+import java.util.Calendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ReportAccountingDepartmentTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 1000);
+        store.add(worker);
+        Report engine = new ReportAccountingDepartment(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary() / 74).append("$").append(";")
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
+
---
+++ src/test/java/ru/job4j/srp/ReportHTMLTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.srp;
+
+import org.junit.Test;
+import ru.job4j.ood.srp.Employee;
+import ru.job4j.ood.srp.MemStore;
+import ru.job4j.ood.srp.Report;
+import ru.job4j.ood.srp.ReportHTML;
+
+import java.util.Calendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ReportHTMLTest {
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportHTML(store);
+        StringBuilder expect = new StringBuilder();
+        expect.append("<?xml version= 1.1 encoding = UTF-8 ?>");
+        expect.append(System.lineSeparator());
+        expect.append("<name> ").append(worker.getName()).append(" <name/>");
+        expect.append(System.lineSeparator());
+        expect.append("<hired> ").append(worker.getHired()).append(" <hired/>");
+        expect.append(System.lineSeparator());
+        expect.append("<fired> ").append(worker.getFired()).append(" <fired/>");
+        expect.append(System.lineSeparator());
+        expect.append("<salary> ").append(worker.getSalary()).append(" <salary/>");
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
