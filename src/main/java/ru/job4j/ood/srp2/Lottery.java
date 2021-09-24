3128c26940ccd6d0e8e7c7e147c0f063bb7a2880
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-24 12:14:43 +0700

    Delete Lottery.java

--- src/main/java/ru/job4j/ood/srp2/Lottery.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.ood.srp2;
-
-import java.time.LocalDate;
-
-public interface Lottery {
-    LocalDate getLotteryDate();
-    int numberOfTicketsSold(int soldTickets);
-    void convertSql(int numberOfTicketsSold, LocalDate dataLottery);
-}
