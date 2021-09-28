95ac83c9f4e2e61e58e3f384febbb1ac4aa0847c
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-28 12:28:25 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ru/job4j/ood/srp/ReportJSON.java

--- src/main/java/ru/job4j/ood/srp/Employee.java
+++ src/main/java/ru/job4j/ood/srp/Employee.java
@@ -68,7 +68,7 @@ public class Employee {
     @Override
     public boolean equals(Object o) {
 
-        if (this == o) {
+        if (this == o)  {
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
