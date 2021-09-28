718ad8f703b16434b93a7914d965c7eab7fc5b36
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-28 13:12:30 +0700

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #208157]

--- src/main/java/ru/job4j/ood/srp/Employee.java
+++ src/main/java/ru/job4j/ood/srp/Employee.java
@@ -4,6 +4,7 @@ import javax.xml.bind.annotation.*;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Objects;
+
 @XmlRootElement(name = "employee")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Employee {
