eda41ac914aa850f09b3217b0396946c9c11a239
Stilan <40999153+Stilan@users.noreply.github.com>
2021-09-29 13:25:32 +0700

    0. Принцип открытости закрытости [#4914 #208090]

--- src/main/java/ru/job4j/ood/ocp/ProfessionsInheritance2.java
+++ src/main/java/ru/job4j/ood/ocp/ProfessionsInheritance2.java
@@ -1,5 +1,9 @@
 package ru.job4j.ood.ocp;
 
+/**
+ *  Класс программист спроектирован неверно если нам нужно создать класс строитель у него тоже есть метод навыки,
+ *  нужен интерфейс.
+ */
 public class ProfessionsInheritance2 {
 
     public static class Programmer {
--- src/main/java/ru/job4j/ood/ocp/StateInheritance3.java
+++ src/main/java/ru/job4j/ood/ocp/StateInheritance3.java
@@ -1,5 +1,8 @@
 package ru.job4j.ood.ocp;
 
+/**
+ * Класс спроектирован неправильно штатов много и у них есть метод численность населения, нужен интерфейс
+ */
 public class StateInheritance3 {
 
     public static class Montana {
