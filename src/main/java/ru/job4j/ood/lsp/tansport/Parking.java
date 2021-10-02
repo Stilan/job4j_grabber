d6537d0ee2b84d8d6df2cb9e81b0bb4b95c1cc85
Stilan <40999153+Stilan@users.noreply.github.com>
2021-10-02 14:29:05 +0700

    2. Парковка машин [#853 #210903]

--- src/main/java/ru/job4j/ood/lsp/tansport/Parking.java
+++ src/main/java/ru/job4j/ood/lsp/tansport/Parking.java
@@ -1,6 +1,9 @@
 package ru.job4j.ood.lsp.tansport;
 
+import java.util.List;
+
 public interface Parking {
     boolean toPark(Car car);
     void add(Car car);
+    List<Car> getCarList();
 }
---
+++ src/test/java/ru/job4j/ood/lsp/tansport/SimpleParkingTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.ood.lsp.tansport;
+
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SimpleParkingTest  {
+
+     @Test
+    public void toParkTest() {
+          SimpleParking simpleParking = new SimpleParking();
+          Car car1 = new Truck(5);
+          Car car2 = new PassengerCars(1);
+          assertThat(simpleParking.toPark(car1), is(true));
+          assertThat(simpleParking.toPark(car2), is(true));
+     }
+     @Test
+     public void addTest() {
+          SimpleParking simpleParking = new SimpleParking();
+          Car car1 = new Truck(5);
+          Car car2 = new PassengerCars(1);
+          simpleParking.add(car1);
+          simpleParking.add(car2);
+          List<Car> carList = List.of(car1, car2);
+          assertThat(simpleParking.getCarList(), is(carList));
+     }
+}
